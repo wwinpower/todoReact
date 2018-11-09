8af3c4d3f629dd197b079c6e32f26d8d952f9513
vladimir.abrahamyan96 <vladimir.abrahamyan96@gmail.com>
2018-11-10 01:30:21 +0400

    Creat Todo React project

---
+++ src/components/app/app.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+
+import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
+import TodoList from '../todo-list';
+import ItemStatusFilter from '../item-status-filter';
+
+import './app.css';
+
+export default class App extends Component {
+
+    state = {
+        todoData: [
+            {label: 'Drink Coffee', important: false, id: 1},
+            {label: 'Make Awesome App', important: true, id: 2},
+            {label: 'Have a lunch', important: false, id: 3}
+        ]
+    };
+
+
+    deleteItem = (id) => {
+        this.setState(({todoData}) => {
+            const idx = todoData.findIndex((el) => el.id === id);
+            const newArray = [
+                ...todoData.slice(0, idx),
+                ...todoData.slice(idx + 1)
+            ];
+
+            return {
+                todoData: newArray
+            };
+        });
+    };
+
+    render() {
+
+        const {todoData} = this.state;
+        return (
+            <div className="todo-app">
+                <AppHeader toDo={1} done={3}/>
+                <div className="top-panel d-flex">
+                    <SearchPanel/>
+                    <ItemStatusFilter/>
+                </div>
+
+                <TodoList
+                    todos={todoData}
+                    onDeleted={this.deleteItem}
+                />
+            </div>
+        );
+    }
+}
